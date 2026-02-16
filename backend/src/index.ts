64da293da413d13216c9cb6a4c08ecd8174b8fd3
JustUzair <justuzairsaiyed@gmail.com>
2026-02-16 19:41:02 +0530

    feat: added express router with rate-limiting on api routes

---
+++ backend/src/index.ts
@@ -0,0 +1,74 @@
+import rateLimit from "express-rate-limit";
+import express from "express";
+import graphRouter from "./routes/graph";
+
+const limiter = rateLimit({
+  limit: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: "Too many requests from this IP, please try again in an hour",
+  handler: (req, res) => {
+    res.status(429).json({
+      status: "fail",
+      message: "Too many requests from this IP, please try again later.",
+      retryAfter: new Date(Date.now() + 60 * 60 * 1000).toLocaleString(
+        "en-IN",
+        {
+          timeZone: "IST",
+        },
+      ),
+    });
+  },
+});
+const app = express();
+app.enable("trust proxy");
+
+app.use("/api", limiter);
+
+app.use("/status", (_, res) => {
+  res.status(200).json({
+    status: "ok",
+    timestamp: new Date().toLocaleString("en-IN", {
+      timeZone: "IST",
+    }),
+  });
+});
+
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+app.use("/api/v1/graph", graphRouter);
+
+// Handling the unhandled routes
+app.all("*", (req, res, next) => {
+  next(new Error(`URL ${req.originalUrl} does not exist on this server !!!`));
+});
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+/*  
+    |----------------------------------------------------------------------|
+    |   SIGTERM is a signal that is sent to request the process terminates |
+    |   that were rejected whose rejections have not yet been handled.     |  
+    |   In other words, it is used for graceful shutdown of server.        |
+    |----------------------------------------------------------------------|
+*/
+process.on("SIGTERM", () => {
+  // SIGTERM - signal fired when Heroku dynos restart
+  console.log("SIGTERM RECEIVED! Shutting Down gracefully!");
+  server.close(() => {
+    console.log("Process Terminated");
+  });
+});
